dfc2340fb0aaa7e002a97b42fa283444b978c78b
BChuuu <cantillonbea2@gmail.com>
2025-10-28 02:37:22 +0800

    Create game_record placeholder data.sql

---
+++ game_record placeholder data.sql
@@ -0,0 +1,8 @@
+INSERT INTO game_record(game_id, title, genre, release_date, platform, price, total_bought, status, review_average, reviews, publisher_id)
+VALUES
+(1101, "Persona 3 Reload", "Role-Playing Game", "2024-02-02", "PC & Console", 2399.00, 200, "active", 5.0, "[review]", 11),
+(1102, "Metaphor: reFantazio", "Role-Playing Game", "2024-10-11", "PC & Console", 2895.00, 200, "active", 4.5, "[review]", 11),
+(1201, "Tekken 8", "Fighting Game", "2024-01-26", "PC & Console", 2499.00, 300, "active", 3.0, "[review]", 12),
+(1301, "Hearts of Iron IV", "Strategy Game", "2016-06-06", "PC", 1999.00, 150, "active", 4.5, "[review]", 13),
+(1401, "Grand Theft Auto VI", "Action-Adventure", "2026-05-26", "Console", 0, 0, "under dev", 0, "no reviews", 14)
+;
